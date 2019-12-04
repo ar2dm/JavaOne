fba1d39b5a84ace516dd412c1136e6d844a48e0f
ar2dm <ar2dm@mail.ru>
2019-12-04 19:11:45 +0300

    2. Создать переменные всех пройденных типов данных, и инициализировать их значения;

--- src/ru/mail/ar2dm/Lesson1/MyClass.java
+++ src/ru/mail/ar2dm/Lesson1/MyClass.java
@@ -2,6 +2,14 @@ package ru.mail.ar2dm.Lesson1;
 
 public class MyClass {
     public static void main(String[] args) {
-
+        byte b=10;
+        short s=2404;
+        int i=123456;
+        long l=1500L;
+        float f=120.0f;
+        double d=15.72775;
+        boolean bool=true;
+        char c='X';
+        String str = "String";
     }
 }
