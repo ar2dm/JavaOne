9f68e9cfcf076ef0ed53e8b5ca0fe1c5b7a7a893
ar2dm <ar2dm@mail.ru>
2019-12-08 20:33:52 +0300

    1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0

--- src/ru/mail/ar2dm/Lesson2/MyClass.java
+++ src/ru/mail/ar2dm/Lesson2/MyClass.java
@@ -2,6 +2,13 @@ package ru.mail.ar2dm.Lesson2;
 
 public class MyClass {
     public static void main(String[] args) {
-
+        int [] nums2 = {0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1};
+        for (int i = 0; i < nums2.length; i++) {
+            if (nums2[i] == 0) {
+                nums2[i] = 1;
+            }
+            else
+                nums2[i] = 0;
+        }
     }
 }
