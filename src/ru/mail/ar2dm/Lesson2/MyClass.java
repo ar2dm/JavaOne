c8113d3d2fc3e43b09dc27a41ba7b6705c842ea5
ar2dm <ar2dm@mail.ru>
2019-12-08 20:48:40 +0300

    7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи нельзя пользоваться вспомогательными массивами.

--- src/ru/mail/ar2dm/Lesson2/MyClass.java
+++ src/ru/mail/ar2dm/Lesson2/MyClass.java
@@ -62,4 +62,28 @@ public class MyClass {
         }
         return check;
     }
+
+    public static void arrayShift (int [] arr, int n){
+        int storage;
+        System.out.println("Shift on " + n);
+        n = n % arr.length; //Чтобы не "гонять" цикл лишнее количество раз.
+        if (n > 0) {
+            for (int j = 0; j < n; j++) {
+                storage = arr[arr.length - 1];
+                for (int i = (arr.length - 1); i > 0; i--) {
+                    arr[i] = arr[i - 1];
+                }
+                arr[0] = storage;
+            }
+        }
+        else if (n < 0){
+            for (int j = n; j < 0; j++ ) {
+                storage = arr[0];
+                for (int i = 0; i < (arr.length - 1); i++) {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.length - 1] = storage;
+            }
+        }
+    }
 }
